01d08f3954f288660bc5dc0b8a8ffc4249ee3e56
TeruyaHigashi <teruyahigashi.65535@gmail.com>
2021-11-25 19:15:40 +0900

    sortのテストパターンを追加

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -6,37 +6,62 @@ import (
 	"time"
 )
 
-const testDataRange = 100
+const testDataAbs = 100
 
 func TestInsertionSort(t *testing.T) {
 	sortingTest(InsertionSort, t)
 }
 
 func sortingTest(sortingFunc func([]int) []int, t *testing.T) {
-	expected := createTestData()
-	actual := sortingFunc(expected)
 
-	if len(actual) != len(expected) {
-		t.Errorf("Length is different.")
-		t.Errorf("expected:%d", len(expected))
-		t.Errorf("actual  :%d", len(actual))
-	}
+	testData := createTestData()
+	for _, unsorted := range testData {
 
-	if len(actual) > 1 {
-		for i := 1; i < len(actual); i++ {
-			if actual[i-1] > actual[i] {
+		actual := sortingFunc(unsorted)
+
+		if len(actual) != len(unsorted) {
+			t.Errorf("Length is different.")
+			t.Errorf("expected:%d", len(unsorted))
+			t.Errorf("actual  :%d", len(actual))
+		}
+
+		if len(actual) == 1 {
+			if actual[0] != unsorted[0] {
+				t.Errorf("Value has changed")
 				t.Errorf("not sorted")
 				t.Errorf("actual : %v", actual)
+			}
+		}
+
+		if len(actual) > 1 {
+			for i := 1; i < len(actual); i++ {
+				if actual[i-1] > actual[i] {
+					t.Errorf("Not sorted")
+					t.Errorf("actual : %v", actual)
+				}
 			}
 		}
 	}
 }
 
-func createTestData() []int {
+func createTestData() [][]int {
+
+	var testData [][]int
+	testData = append(testData, []int{}, []int{1})
+
 	rand.Seed(time.Now().UnixNano())
-	var testData []int
-	for i := 0; i < testDataRange; i++ {
-		testData = append(testData, int(rand.Intn(testDataRange+testDataRange)-testDataRange))
+	var onlyPositives []int
+	for i := 0; i < testDataAbs; i++ {
+		onlyPositives = append(onlyPositives, int(rand.Intn(testDataAbs)))
 	}
+	testData = append(testData, onlyPositives)
+
+	rand.Seed(time.Now().UnixNano())
+	var includedNegatives []int
+	for i := 0; i < testDataAbs; i++ {
+		includedNegatives = append(includedNegatives, int(rand.Intn(testDataAbs+testDataAbs)-testDataAbs))
+	}
+	testData = append(testData, includedNegatives)
+
 	return testData
 }
