3c6efe57159ee29aed5d278b1df6a57542b6078a
TeruyaHigashi <teruyahigashi.65535@gmail.com>
2021-12-03 20:16:18 +0900

    stackのテストを修正

--- structure/stack/stack_test.go
+++ structure/stack/stack_test.go
@@ -1,54 +1,78 @@
 package stack
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStackSlice(t *testing.T) {
 
 	stack := &StackSlice{}
 	lengthTest(stack, 0, t)
+
+	stack = &StackSlice{}
 	isEmptyTest(stack, true, t)
+
+	stack = &StackSlice{}
 	pushTest(stack, t)
+
+	stack = NewStackSlice("aaa", "bbb", 123)
+	lengthTest(stack, 3, t)
+
+	stack = NewStackSlice("aaa", "bbb", 123)
+	isEmptyTest(stack, false, t)
+
+	stack = NewStackSlice("aaa", "bbb", 123)
+	peekTest(stack, 123, t)
+
+	stack = NewStackSlice("aaa", "bbb", 123)
 	popTest(stack, t)
 
-	stack = stack.NewStackSlice("aaa", "bbb", 123)
-	lengthTest(stack, 3, t)
-	peekTest(stack, 123, t)
-	isEmptyTest(stack, false, t)
+	stack = NewStackSlice("aaa", "bbb", 123)
 	pushTest(stack, t)
-	popTest(stack, t)
 }
 
 func TestStackLinkedList(t *testing.T) {
 
 	stack := &StackLinkedList{}
 	lengthTest(stack, 0, t)
+
+	stack = &StackLinkedList{}
 	isEmptyTest(stack, true, t)
+
+	stack = &StackLinkedList{}
 	pushTest(stack, t)
+
+	stack = NewStackLinkedList("aaa", "bbb", 123)
+	lengthTest(stack, 3, t)
+
+	stack = NewStackLinkedList("aaa", "bbb", 123)
+	isEmptyTest(stack, false, t)
+
+	stack = NewStackLinkedList("aaa", "bbb", 123)
+	peekTest(stack, 123, t)
+
+	stack = NewStackLinkedList("aaa", "bbb", 123)
 	popTest(stack, t)
 
-	stack = stack.NewStackLinkedList("aaa", "bbb", 123)
-	lengthTest(stack, 3, t)
-	peekTest(stack, 123, t)
-	isEmptyTest(stack, false, t)
+	stack = NewStackLinkedList("aaa", "bbb", 123)
 	pushTest(stack, t)
-	popTest(stack, t)
 }
 
 func lengthTest(stack Stack, expected int, t *testing.T) {
 	if stack.Length() != expected {
-		t.Errorf("Lengthが異常")
+		t.Errorf("Lengthが期待値と相違")
 	}
 }
 
 func isEmptyTest(stack Stack, expected bool, t *testing.T) {
 	if stack.IsEmpty() != expected {
-		t.Errorf("IsEmptyが異常")
+		t.Errorf("IsEmptyが期待値と相違")
 	}
 }
 
 func peekTest(stack Stack, expected interface{}, t *testing.T) {
 	if stack.Peek() != expected {
-		t.Errorf("Peekが異常")
+		t.Errorf("Peekが期待値と相違")
 	}
 }
 
@@ -58,10 +82,10 @@ func pushTest(stack Stack, t *testing.T) {
 	stack.Push(1)
 
 	if stack.Length() != expectedLength {
-		t.Errorf("Push後のlengthが異常")
+		t.Errorf("Push後のlengthが期待値と相違")
 	}
 	if stack.Peek() != 1 {
-		t.Errorf("Push後のtopが異常")
+		t.Errorf("Push後のtopが期待値と相違")
 	}
 }
 
@@ -72,9 +96,9 @@ func popTest(stack Stack, t *testing.T) {
 	actual := stack.Pop()
 
 	if stack.Length() != expectedLength {
-		t.Errorf("Pop後のlengthが異常")
+		t.Errorf("Pop後のlengthが期待値と相違")
 	}
 	if actual != expectedPoped {
-		t.Errorf("Popした値が異常")
+		t.Errorf("Popした値が期待値と相違")
 	}
 }
